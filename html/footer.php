ef048da99c87c09ae774d9f63fc3f74d4e5ba471
Sher0193 <ovidvscape@gmail.com>
2020-07-27 00:05:47 -0400

    Fix Mismatched HTML Tag

--- html/footer.php
+++ html/footer.php
@@ -1,4 +1,4 @@
     <div class="footer-text">
-        <a href="mailto: dylansherwood11@gmail.com" style="text-decoration: none" >DYLAN SHERWOOD - dylansherwood11@gmail.com</p>
+        <a href="mailto: dylansherwood11@gmail.com" style="text-decoration: none" >DYLAN SHERWOOD - dylansherwood11@gmail.com</a>
     </div> <!-- end footer text -->
 </html>
